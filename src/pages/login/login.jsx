13acc7b26739e62205359273f556d0f437e0c6d7
maiyzaza <maiy27982@gmail.com>
2021-12-30 02:30:47 +0700

    asdasd

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -24,11 +24,17 @@ function Login() {
     }
     axios.post("https://arr-dev.azurewebsites.net/api/v1/login/login", payload)
     .then((res) => {
+<<<<<<< HEAD
       // console.log("res",res.data.data.token);
       console.log(res.data.data);
       window.sessionStorage.setItem("token", res.data.data.token)
       history.push("/roomManagement")
       // window.location.reload()
+=======
+      console.log("res",res.data.data.token);
+      window.sessionStorage.setItem("token", res.data.data.token)
+      history.push("/roomManagement")
+>>>>>>> 09905bd (finish login page)
       setAlert(false)
     })
     .catch((res) => {
@@ -37,30 +43,48 @@ function Login() {
   }
 
   return (
+<<<<<<< HEAD
     <div className="login_container">
+=======
+    <div>
+>>>>>>> 927a603 (pull origin develop)
       <form class="login_form" onSubmit={handleSubmit}>
           <img class="login_logo" src={logoImg}></img>
           <input
             onChange={event => setUsername(event.target.value)}
             value={username}
+<<<<<<< HEAD
             required
+=======
+            // required
+>>>>>>> 927a603 (pull origin develop)
             id="username"
             name="Username"
             placeholder="Username"
           />
           <input
+<<<<<<< HEAD
             required
             onChange={event => setPassword(event.target.value)}
             value={password}
             type="password"
+=======
+            // required
+            onChange={event => setPassword(event.target.value)}
+            value={password}
+>>>>>>> 927a603 (pull origin develop)
             id="password"
             name="Password"
             placeholder="Password"
           />
           <button>Sign In</button>
+<<<<<<< HEAD
           <div class="login_wrong">
           {alert && <p> Wrong Username or Password</p>}
           </div>
+=======
+          {alert && <p> Worng Username or Password</p>}
+>>>>>>> 927a603 (pull origin develop)
       </form>
     </div>
   );
